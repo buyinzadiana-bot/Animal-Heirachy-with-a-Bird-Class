e8d823597da2fa8d9064f9d26ecfa08ca6616529
Diana BUYINZA <buyinzadiana@gmail.com>
2026-02-09 15:39:17 +0200

    created a Bird class

---
+++ src/Bird.java
@@ -0,0 +1,10 @@
+public class Bird extends Animal{
+    public void Fly() {
+        System.out.println("bird does: flying!");}
+
+        @Override
+                public void makeSound(){
+            System.out.println(" Bird says:Chirps");
+        }
+
+}
